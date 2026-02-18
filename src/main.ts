25c6ac44ed395f33413af3d643402f41360fad21
svkaka <mr.svkaka@gmail.com>
2026-02-18 02:14:33 +0100

    Support for folders

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import {Plugin, TFile, Notice, Menu, TAbstractFile} from 'obsidian';
+import {Plugin, TFile, Notice, Menu, TAbstractFile, TFolder} from 'obsidian';
 import {DEFAULT_SETTINGS, MyPluginSettings} from "./settings";
-
-// Remember to rename these classes and interfaces!
 
 export default class CopyFilesContent extends Plugin {
 	settings: MyPluginSettings;
@@ -46,19 +44,28 @@ export default class CopyFilesContent extends Plugin {
 	}
 
 	async copySelectedFiles(files: TAbstractFile[]): Promise<void> {
-		// TODO Support folders
-		const validFiles = files.filter((f): f is TFile => f instanceof TFile);
+		const visited: Set<string> = new Set
+		const cleanedSelectedFiles: TFile[] = []
+		// respect the order of selection
+		this.getFilesRecursive(files).forEach((file) => {
+			if (!visited.has(file.path)) {
+				visited.add(file.path)
+				cleanedSelectedFiles.push(file)
+			}
+		})
 
-		if (validFiles.length === 0) {
-			new Notice("No files selected.");
+		if (cleanedSelectedFiles.length === 0) {
+			new Notice("No .md files found.");
 			return;
 		}
 
-		const readPromises = validFiles.map(async (file) => {
+		const readPromises = cleanedSelectedFiles.map(async (file) => {
 			try {
 				const content = await this.app.vault.read(file);
-				//  TODO support Formatting
-				return `# ${file.basename}\n\n${content}\n\n---\n\n`;
+				//TODO custom header
+				const breadcrumb = file.path.replace(`.${file.extension}`, '');
+
+				return `# ${breadcrumb}\n\n${content}\n\n---\n\n`;
 			} catch (err) {
 				console.error(`Failed to read ${file.path}`, err);
 				return null;
@@ -70,9 +77,28 @@ export default class CopyFilesContent extends Plugin {
 
 		if (finalContent) {
 			await navigator.clipboard.writeText(finalContent);
-			new Notice(`Copied ${validFiles.length} files to clipboard!`);
+			new Notice(`Copied ${cleanedSelectedFiles.length} files to clipboard!`);
 		} else {
 			new Notice("Failed to copy content.");
 		}
+	}
+
+	/**
+	 * Recursive helper to flatten folders into a list of files.
+	 */
+	getFilesRecursive(files: TAbstractFile[]): TFile[] {
+		let result: TFile[] = [];
+
+		for (const file of files) {
+			if (file instanceof TFile) {
+				// TODO support other types
+				if (file.extension === 'md') {
+					result.push(file);
+				}
+			} else if (file instanceof TFolder) {
+				result = result.concat(this.getFilesRecursive(file.children));
+			}
+		}
+		return result;
 	}
 }
